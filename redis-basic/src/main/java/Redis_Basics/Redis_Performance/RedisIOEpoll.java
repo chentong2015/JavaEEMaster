9e35c577e844facbe689b15c6d3dd948a8114d4b
Tong Chen <tong.chen@etu.utc.fr>
2021-09-18 21:39:16 +0200

    commit redis basic project

--- redis-basic/src/main/java/Redis_Basics/Redis_Performance/RedisIOEpoll.java
+++ redis-basic/src/main/java/Redis_Basics/Redis_Performance/RedisIOEpoll.java
@@ -1,9 +1,11 @@
 package Redis_Basics.Redis_Performance;
 
-// Linux IO多路复用核心原理: https://blog.csdn.net/XueyinGuo/article/details/113096163
-// aeApiCreate() { epoll_create(); ... }
-// aeApiPoll() { epoll_wait(); ... }
-// aeApiDelEvent() { epoll_ctl(); ... }
+// Linux IO多路复用核心原理:
+// https://blog.csdn.net/XueyinGuo/article/details/113096163
 public class RedisIOEpoll {
-    
+
+    // IO多路复用解决的问题
+    // int iResult = recv(socket_fd, buffer, 1024);
+    // 1. 一个server只能连接一个socket套接字
+    // 2. 永久阻塞IO，只有一个client socket能够连接成功
 }
