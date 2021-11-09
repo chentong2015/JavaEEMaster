629da4d7d0d77c027a25695d81ee6821dc5203b2
Tong Chen <tong.chen@etu.utc.fr>
2021-11-09 16:48:44 +0100

    clear serverless framework

--- serverless-framework/src/main/java/spring_cloud/BaseSpringCloud.java
+++ serverless-framework/src/main/java/spring_cloud/BaseSpringCloud.java
@@ -1,10 +1,5 @@
 package spring_cloud;
 
-// 掌握到源码级别  https://spring.io/projects/spring-cloud
-// SpringCloud最新实战视频       https://www.bilibili.com/video/BV12v411L7o5
-// Spring Cloud和Eureka源码理解  https://www.bilibili.com/video/BV1yJ411g7YS
-
-// Spring Cloud权限管理系统：服务网关Zuul https://www.cnblogs.com/xifengxiaoma/p/9916363.html
 public class BaseSpringCloud {
 
 }
