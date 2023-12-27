122859b3e1e9805d5e9e1fd092890f0c4098b498
chen tong <chen2015tong@gmail.com>
2023-12-27 17:03:25 +0100

    update project for mac os

--- remote-procedure-call/rpc-server/src/main/java/com/rpc/server/MainServer.java
+++ remote-procedure-call/rpc-server/src/main/java/com/rpc/server/MainServer.java
@@ -13,14 +13,15 @@ public class MainServer {
     // RPC服务端对外暴露socket接口，接收另外进程来的请求
     // 使用异步的方式来处理请求，避免RPC服务端在接收请求时被阻塞
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(1234);
-        Executor executor = Executors.newFixedThreadPool(10);
-        while (true) {
-            System.out.println("wait connection");
-            Socket socket = serverSocket.accept();
-            System.out.println("connection ok");
-            // 执行通讯成功的socket
-            executor.execute(new RequestHandler(socket));
-        }
+       try(ServerSocket serverSocket = new ServerSocket(1234)){
+           Executor executor = Executors.newFixedThreadPool(10);
+           while (true) {
+               System.out.println("wait connection");
+               Socket socket = serverSocket.accept();
+               System.out.println("connection ok");
+               // 执行通讯成功的socket
+               executor.execute(new RequestHandler(socket));
+           }
+       }
     }
 }
